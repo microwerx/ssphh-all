721a145a9552bbaed674671639a3b4cfc78ee677
microwerx <jmetzgar@outlook.com>
2020-11-11 14:04:36 -0700

    update submodules

---
+++ gitsync.ps1
@@ -0,0 +1,9 @@
+git -C fluxions pull
+git -C fluxions-gte pull
+git -C fluxions-base pull
+git -C damselfish pull
+git -C hatchetfish pull
+git -C starfish pull
+git -C unicornfish pull
+git -C viperfish pull
+
